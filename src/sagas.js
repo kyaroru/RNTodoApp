98ba5e0865aa70d0a26963657f3bc69379be367d
Chiew Carol <kyaroru@outlook.com>
2017-05-02 11:18:40 +0800

    Added codepush integration :p

--- src/sagas.js
+++ src/sagas.js
@@ -1,8 +1,10 @@
-import { fork } from 'redux-saga/effects';
+import { fork, spawn } from 'redux-saga/effects';
 import todo from './components/todo-list';
+import codePushSaga from 'react-native-code-push-saga';
 
 export default function* root() {
   yield [
     fork(todo.saga),
+    yield spawn(codePushSaga),
   ];
 }
